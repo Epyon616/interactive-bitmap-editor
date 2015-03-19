fc1f93f0b4372b167a645404ce3960bf8c2035ac
Lee Richmond <lee@stealthygecko.com>
2015-03-19 22:27:55 +0000

    Add Clear method
    * Add Clear method to set the colour to 'O'
    * Add a getter to help check the pixel colour

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,7 +1,16 @@
 class Bitmap
-  attr_reader :cols, :rows
+  attr_reader :cols, :rows, :pixels
 
   def initialize(cols, rows)
     @cols, @rows = cols, rows
+    clear
+  end
+
+  def clear
+    @pixels = (1..@rows).map{ (1..@cols).map{"O"}}
+  end
+
+  def get_pixel_colour(x,y)
+    @pixels[x - 1][y - 1]
   end
 end
--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -9,4 +9,18 @@ describe "Bitmap" do
     it { expect(subject.cols).to eql(2) }
     it { expect(subject.rows).to eql(2) }
   end
+
+  describe "#clear" do
+    it "sets colour back to 'O'" do
+      (1..subject.cols).map do |col|
+        (1..subject.rows).map do |row|
+          expect(subject.get_pixel_colour(col, row)).to eql("O")
+        end
+      end
+    end
+  end
+
+  describe "#get_pixel_colour" do
+    it {expect(subject.get_pixel_colour(1,1)).to eql("O")}
+  end
 end
