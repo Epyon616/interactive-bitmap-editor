40fa9e9ec4c29025fd3c94a6170380e617f3bcf8
Lee Richmond <lee@stealthygecko.com>
2015-03-23 12:28:02 +0000

    Make tests more readable

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -3,52 +3,51 @@ require_relative "../lib/bitmap"
 describe "Bitmap" do
   let(:cols) { 2 }
   let(:rows) { 2 }
-  subject { Bitmap.new(cols, rows) }
+  subject(:large_bitmap) { Bitmap.new(4, 4) }
+  subject(:bitmap) { Bitmap.new(cols, rows) }
 
   describe ".intialize" do
-    it { expect(subject.cols).to eql(2) }
-    it { expect(subject.rows).to eql(2) }
+    it { expect(bitmap.cols).to eql(2) }
+    it { expect(bitmap.rows).to eql(2) }
   end
 
   describe "#clear" do
     it "sets colour back to 'O'" do
-      (1..subject.cols).map do |col|
-        (1..subject.rows).map do |row|
-          expect(subject.get_pixel_colour(col, row)).to eql("O")
+      (1..bitmap.cols).map do |col|
+        (1..bitmap.rows).map do |row|
+          expect(bitmap.get_pixel_colour(col, row)).to eql("O")
         end
       end
     end
   end
 
   describe "#get_pixel_colour" do
-    it { expect(subject.get_pixel_colour(1,1)).to eql("O") }
+    it { expect(bitmap.get_pixel_colour(1,1)).to eql("O") }
   end
 
   describe "#set_pixel_colour" do
-    before { subject.set_pixel_colour(2, 2, "C") }
-    it { expect(subject.get_pixel_colour(2,2)).to eql("C") }
+    before { bitmap.set_pixel_colour(2, 2, "C") }
+    it { expect(bitmap.get_pixel_colour(2,2)).to eql("C") }
   end
 
   describe "#draw_vertical_segment" do
-    before { subject.draw_vertical_segment(2, 1, 2, "C") }
+    before { bitmap.draw_vertical_segment(2, 1, 2, "C") }
 
-    it { expect(subject.get_pixel_colour(2, 1)).to eql("C") }
-    it { expect(subject.get_pixel_colour(2, 2)).to eql("C") }
-    it { expect(subject.get_pixel_colour(1, 1)).to eql("O") }
+    it { expect(bitmap.get_pixel_colour(2, 1)).to eql("C") }
+    it { expect(bitmap.get_pixel_colour(2, 2)).to eql("C") }
+    it { expect(bitmap.get_pixel_colour(1, 1)).to eql("O") }
   end
 
   describe "#draw_horizontal_segment" do
-    before { subject.draw_horizontal_segment(2, 1, 2, "C") }
+    before { bitmap.draw_horizontal_segment(2, 1, 2, "C") }
 
-    it { expect(subject.get_pixel_colour(1, 1)).to eql("O") }
-    it { expect(subject.get_pixel_colour(1, 2)).to eql("C") }
-    it { expect(subject.get_pixel_colour(2, 2)).to eql("C") }
+    it { expect(bitmap.get_pixel_colour(1, 1)).to eql("O") }
+    it { expect(bitmap.get_pixel_colour(1, 2)).to eql("C") }
+    it { expect(bitmap.get_pixel_colour(2, 2)).to eql("C") }
   end
 
 
   describe "#fill_region" do
-    let(:large_bitmap) { Bitmap.new(4, 4) }
-
     before do
       large_bitmap.draw_horizontal_segment(2, 2, 3, "R")
       large_bitmap.draw_horizontal_segment(3, 2, 3, "R")
